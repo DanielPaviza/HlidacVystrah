9ea448588e2c0c5c82faf870fe4f4587e9be6652
Daniel Pavíza <daniel.paviza@gmail.com>
2023-12-07 11:45:08 +0100

    init

---
+++ hlidacVystrah/Controllers/AlertsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+using System.Xml.Linq;
+using hlidacVystrah.Model;
+
+namespace hlidacVystrah.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlertsController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private string url;
+
+        public AlertsController(ILogger<WeatherForecastController> logger)
+        {
+            this._logger = logger;
+            this.url = @"D:\moje\programovani\absolutorium\test_data\clean.xml";
+        }
+
+        [HttpGet]
+        public AlertsResponse Get()
+        {
+
+            try
+            {
+                // Load the XML file using XDocument
+                XDocument xdoc = XDocument.Load(this.url);
+                IEnumerable<XElement> alertElements = xdoc.Root.Elements();
+
+                string? dataTimestamp = alertElements.FirstOrDefault(el => el.Name.LocalName == "sent").Value;
+                List<XElement> infoElements = alertElements.Where(el => el.Name.LocalName == "info").ToList();
+
+                return new AlertsResponse
+                {
+                    responseCode = 200,
+                    dataTimestamp = dataTimestamp,
+                    alerts = infoElements
+                };
+            }
+            catch (Exception ex)
+            {
+                return new AlertsResponse
+                {
+                    responseCode = 500,
+                    dataTimestamp = ex.Message,
+                };
+            }
+
+            return new AlertsResponse
+            {
+                responseCode = 200,
+                dataTimestamp = "",
+            };
+        }
+
+        private XElement FindElementByName(IEnumerable<XElement> elements, string name) {
+            return elements.Where(el => el.Name.LocalName == name).FirstOrDefault();
+        }
+    }
+}
---
+++ hlidacVystrah/Model/AlertsResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Linq;
+
+namespace hlidacVystrah.Model
+{
+    public class AlertsResponse
+    {
+
+        public int responseCode { get; set; }
+
+        public string dataTimestamp { get; set; }
+
+        public List<XElement> alerts { get; set; } = new();
+    }
+}
